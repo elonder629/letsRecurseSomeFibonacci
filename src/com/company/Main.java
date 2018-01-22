de1f9c1ea29d4ec684c32fd8fd310a2c6cb44db5
elonder629 <ericlonder730@gmail.com>
2018-01-22 08:51:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter to check if a number is a fibonacci number");
+
+    }
+
+    public static boolean fibonacciChecker(int num, String sequence){
+        if (num == 1) {
+            return true;
+        }
+        else if ()
+    }
+}
