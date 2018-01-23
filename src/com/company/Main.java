35ba5f6616c456016594a860a8792b92c37cc459
elonder629 <ericlonder730@gmail.com>
2018-01-23 08:24:57 -0500

    neat code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+public static ArrayList<Integer> fibo = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter to check if a number is a fibonacci number");
+        int temp = input.nextInt();
+        fibo.add(0);
+        fibo.add(1);
+        System.out.println(fibonacciChecker(temp) + "\n" + fibo);
 
     }
 
-    public static boolean fibonacciChecker(int num, String sequence){
-        if (num == 1) {
+    public static boolean fibonacciChecker(int num){
+        fibo.add(fibo.get(fibo.size()-1)+fibo.get(fibo.size()-2));
+        if (fibo.get(fibo.size()-1) > num)
+            return false;
+        else if (fibo.contains(num))
             return true;
-        }
-        else if ()
+        else
+            return fibonacciChecker(num);
     }
 }
